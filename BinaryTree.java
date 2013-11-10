2d3b7ae065af35b6b0808f128826d448e7333893
walsondias <walsondias@yahoo.com>
2013-11-10 18:27:32 -0500

    Update BinaryTree.java
    getheight added

--- BinaryTree.java
+++ BinaryTree.java
@@ -5,5 +5,13 @@ public class Node{
 public class BinaryTree{
 	Node root;
 	
+	/*Get the height of a tree*/
+	public int getHeight(){return getHeight(root);}
+	public int getHeight(Node node){
+		if(node == null)
+			return 0;
+		return 1 + Math.max(hetHeight(node.left), getHeight(node.right));
+	}
+	
 	
 }
