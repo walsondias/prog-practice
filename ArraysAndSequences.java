3da8bda7f2aeabbde01452510c8f3a5be0ac7278
walsondias <walsondias@yahoo.com>
2013-11-11 19:24:54 -0500

    Update ArraysAndSequences.java

--- ArraysAndSequences.java
+++ ArraysAndSequences.java
@@ -173,4 +173,155 @@ public class ArraysAndSequences{
     }
     return res;
    }
+   
+   /*Given an integer array. If the element is +k you can move fwd k steps or 
+   if the value -k you can move backward k steps. You need to check whether you can navigate 
+   from 0 index to last index in the array.*/
+   public boolean isLastReachable(int [] arr){
+    int i = 0, sum = 0;
+    while(i >=0 && i<= arr.length-1){
+        sum += arr[i];
+        if(sum == arr.length -1)
+            return true;
+        else if(sum == 0){
+            return false;
+        }
+        i += arr[i];
+     }
+     return false;
+   }
+   
+   /*Write a program to find whether a word is anagram or not*/
+   public boolean isAnagram(String s1, String s2){
+        if(s1==null || s2==null || s1.length()!=s2.length)
+            return false;
+        char [] s1Arr = s1.toCharArray();
+        char [] s2Arr = s2.toCharArray();
+        Arrays.sort(s1Arr);
+        Arrays.sort(s2Arr);
+        for(int i=0; i<s1Arr.length(); i++){
+            if(s1Arr[i] != s2Arr[i])
+                return false;
+        }
+        return true;
+   }
+   /*Design an algorithm to take a list of strings as well as a single input string, 
+    *and return the indices of the list which are anagrams of the input string, 
+    *disregarding special characters.*/
+    public ArrayList <Integer> getAnagramIndices(String s, String [] arr){
+        s = s.replaceAll("\\W", "");
+        ArrayList <Integer> indices = new ArrayList <Integer> ();
+        for(int i=0; i<arr.length(); i++){
+            if(isAnagram(s, arr[i].replaceAll("\\W", ""))
+                indices.add(i);
+        }
+        return indices;
+    }
+    
+    /*Given an integer N and an array of unsorted integers A 
+     *find all pairs of numbers within A which add up to N*/
+     public void checkIfAddingToN(int n, int [] arr){
+         Arrays.sort(arr);
+         int i=0, j=0;
+         while(i<j){
+            if(arr[i]+arr[j] == n){
+                System.out.println(i + " & " + j);
+                i++;
+                j--;
+            }
+            else if(arr[i]+arr[j] < n){
+                i++;
+            }
+            else{
+                j--;
+            }
+         }
+     }
+     
+     /*Code an algorithm to find the longest palindrome in a string*/
+     public void longestPalindrome(String s){
+         char [] arr = s.toCharArray();
+         int start=0, end=0;
+         for(int i=0; i<arr.length-2; i++){
+             for(int j=i+end-start; j<arr.length-1; j++){
+                 if(isPalindrome(arr, i, j)){
+                     start = i;
+                     end = j;
+                 }
+             }
+         }
+         for(int i=start; i<=end; i++){
+             System.out.print(arr[i]);
+         }
+     }
+     public boolean isPalindrome(char []arr, int start, int end){
+         while(start < end){
+             if(arr[start] == arr[end]){
+                 start++;
+                 end--;
+             }
+             else
+                return false;
+         }
+         return true;
+     }
+     
+     /*Compress a string? If "AAABBAAA" the resultant string should be 3A2B3A.*/
+     public String compress(String s){
+         int count = 1;
+         char past=s.get(0), curr;
+         StringBuilder out = new StringBuilder("");
+         for(int i=1; i<s.length(); i++){
+             curr = s.charAt(i);
+             if (past == curr){
+                 count++;
+             }
+             else{
+                 out.append(count+""+past);
+                 past = curr;
+                 count = 1;
+             }
+         }
+         out.append(count+""+curr);
+         return out.toString();
+     }
+     
+     /*Compute sum of the elements in an integer array recursively*/
+     public int sumOfArray(int [] arr){
+         if(arr.length == 0)
+            return 0;
+        else
+            return arr[1]+sum(arr, 2);
+     }
+     public int sum(int []arr, int index){
+         if(index < arr.length){
+             return arr[index] + sum(arr, index + 1);
+         }
+         else{
+             return 0;
+         }
+     }
+     
+     /*Find the top two largest integers from an unsorted array*/
+     public int [] topTwo(int [] arr){
+         int [] top = new int [2];
+         if(arr==null || arr.size() <=2)
+            return arr;
+        else
+            top[0] = Math.max(arr[0], arr[1]);
+            top[1] = Math.min(arr[0], arr[1]);
+            for(int i=2; i<arr.size; i++){
+                if(arr[i] > top[0]){
+                    top[1] = top[0];
+                    top[0] = arr[i];
+                }
+                else if(arr[i] > top[1]){
+                    top[1] = arr[i];
+                }
+            }
+            return top;
+     }
 }
+
+
+
