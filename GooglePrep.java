7fccb5bd8861f07c4bc82493b450f7a53e996aa5
walsondias <walsondias@yahoo.com>
2014-02-16 20:18:31 -0800

    Update GooglePrep.java

--- GooglePrep.java
+++ GooglePrep.java
@@ -7,18 +7,20 @@ public class GooglePrep{
     }
     public ArrayList<Interval> groupIntervals (ArrayList<Interval> set, Interval newInterval){
         for(int i=0; i<set.size(); i++){
-              if(set.get(i).getEnd().compare(newInterval.getStart()<=0){
+              if(set.get(i).getEnd().compare(newInterval.getStart()>=0
+                && set.get(i).getStart().compare(newInterval.getStart()<=0){
                   newInterval = mergeIntervals(set.get(i), newInterval);
                   set.remove(i);
-              } else if (set.get(i).getStart().compare(newInterval.getEnd()>=0){
+              } else if (set.get(i).getStart().compare(newInterval.getEnd()<=0
+                && set.get(i).getStart().compare(newInterval.getStart()>=0){
                   newInterval = mergeIntervals(newInterval, set.get(i));
                   set.remove(i);
               }
         }
         set.add(newInterval);
     }
-    public Interval mergeIntervals(Interval start, Interval end){
-        return new Interval(start.getStart(), end.getEnd());
+    public Interval mergeIntervals(Interval int1, Interval int2){
+        return new Interval(Math.min(int1.getStart(), int2.getStart()), Math.max(int1.getEnd(),int2.getEnd()));
     }
     
     //Guessing game - I pick a number between 1 and 100 and you are trying to guess it. Every time you query a number I tell you if it is higher or lower.
@@ -95,5 +97,70 @@ public class GooglePrep{
     //that is larger that the original.
     //In this case will be 5421. It has the same digits 1-2-4-5. 
     //And from all the combinations bigger than 5412 is the smallest.
+    public int justLargerOf(int num){
+        int [] digits = new int [Math.ceil(Math.log(num)/Math.log(10))]; //Assume positive number
+        int [] temp = new int [digits.length];
+        int tmp;
+        for(int i=0; i<digits.length; i++){
+            digits[i] = num % 10;
+            num = num/10;
+        } //Get the number in reverse arr
+        int j=0;
+        while(j<digits.length-1 && digits[j]<=digits[j+1]){
+            temp[j] = digits[j];
+            j++;
+        }
+        if(j==digits.length-1){
+            return -1;
+        } else {
+            Arrays.sort(temp);
+            for(int k=0; k<=j; k++){
+                if(temp[k]>digits[j+1]){
+                    tmp = digits[k+1];
+                    digits[k+1] = temp[k];
+                    temp[k] = tmp;
+                    break;
+                }
+            }
+            Arrays.sort(temp);
+            for(int k=0; k<=j; k++){
+                digits[k] = temp[k];
+            }
+            tmp = 0;
+            for(int k=0; k<digits.length; k++){
+                tmp = tmp+digits[k]*Math.pow(10,k);
+            }
+            return tmp;
+        }
+    }
     
+    //given 4 digits array, like 0,0,0,0, plus 1 into this array and print. Ex. 8,8,8,8 to 8,8,8,9. 
+    //consider one situation, 9,9,9,9 to 1,0,0,0,0.
+    public ArrayList <Integer> add(ArrayList <Integer> lst, int num){
+        int carry = num, sum = 0, index = lst.size() - 1;
+        do{
+            sum = lst.get(index) + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            lst.set(index--, sum);
+        } while (index>=0 && carry > 0);
+        if(carry > 0){
+            lst.addFirst(carry);
+        }
+        return lst;
+    }
+    
+    //print all numbers between 1 and a million - with no repeating digits
+    public void printNonRepeating(){
+        
+    }
+    
+    //maximun rectangular area under histogram
+    public maxHistogramArea(int [] hist){
+        Arrays.sort(hist);
+        int max;
+        for(int i=0; i<hist.length; i++){
+            
+        }
+    }
 }
